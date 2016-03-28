3d281a9472b290c14f9e53609e9765ed64a4c558
Chaeyun Yang <ycy6685@naver.com>
2016-03-28 20:11:06 +0900

    #2 Add Result Activity

---
+++ app/src/main/java/kr/hs/emirim/ycy6685/randomamasvin/ResultActivity.java
@@ -0,0 +1,16 @@
+package kr.hs.emirim.ycy6685.randomamasvin;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by appcreator04 on 2016. 3. 28..
+ */
+public class ResultActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
